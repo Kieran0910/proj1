fd3921fb5c06922934d607c94abba54f72118258
kieran Burgess <kswburgess@gmail.com>
2019-04-09 23:16:13 +0000

    working code

--- actualcode.c
+++ actualcode.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
 int main () {
-    int n, x;
+    int n;
     int key; //allows user input for how many spaces to shift message
     char string[100];
     
     printf("Enter your message for encryption\n");
-    scanf("%s", &string);
-    
-    printf("insert your key");
+    scanf(" %[^\n]%*c", string);
+
+    printf("insert your key\n");
     scanf("%d", &key);
     
-    for(n=0;n>x;n++){
+    for( n=0 ; n < strlen(string) ; n++ ){
           string[n] = string[n] + key;
+        
+          
     }
+    printf("Your Encription is: %s\n", string);
   
 
 }

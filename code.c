5dd3987e484d1e177928fab91f41441baefd412f
kieran Burgess <kswburgess@gmail.com>
2019-04-10 01:26:10 +0000

    code been working in the wrong file so not sure where other commits are

--- code.c
+++ code.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
-{
-   int i, x; //declare values 
-   char msge[100] = "ATTACK AT SUNRISE"; // max size of string
-   char key;
-   printf("Please enter a Message use - as a space:\n"); //input from the user
-   scanf("%s",&msge);
-   
-   printf("please enter a key:\n");//users input for key
-   scanf("%d", &key);
+int main () {
+    int n;
+    int key; //allows user input for how many spaces to shift message
+    char s1[100];
+    fgets(s1);
+    
+    printf("Enter your message for encryption\n");
+    scanf(" %s", s1 );
 
-   printf("Select from options below:\n");
-   printf("1 = Encrypt Message.\n");
-   printf("2 = Decrypt Message.\n");
-   scanf("%d", &x);
-
-   
-}
+    printf("insert your key\n");
+    scanf("%d", &key);
+    
+    for( n=0 ; n < strlen(string) ; n++ ){
+          string[n] = string[n] + key;
+        
+          
+    }
+    printf("Your Encription is: %s\n", string);
+  

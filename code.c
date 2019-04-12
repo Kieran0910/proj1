652fc1e92874c1cf52c12b8895b2d781a141dcad
kieran Burgess <kswburgess@gmail.com>
2019-04-12 01:42:37 +0000

    code

--- code.c
+++ code.c
@@ -4,11 +4,11 @@
 int main () {
     int n, input, RS;
     int key; //allows user input for how many spaces to shift message
-    char s1[100];//initilise a string
-    char s2[100]; //only used in substitution for cipher 
-    char s3[100];
-    char f1[100];
-    char f2[200];
+    char s1[250];//initilise a string
+    char s2[250]; //only used in substitution for cipher 
+    char s3[250];
+    char f1[250];
+    char f2[250];
     
     do  {
     printf("\nDo you want to use Substition Cipher \nOr The rotation Cipher\n");
@@ -19,13 +19,12 @@ int main () {
     
     
     if(RS == 1) {
-        printf("Enter your message to encrypt or decrypt:\n"); //use input
+        printf("\nEnter your message to encrypt or decrypt:\n"); //use input
         scanf(" %[^\n]", s1 ); //scans user input into the string created above
-    
         printf("insert your key\n"); //key allows for rotation of given amount
         scanf("%d", &key);
         do  {
-            printf("Select from options below:\n"); //allows user to select what to do with there messege
+            printf("\nSelect from options below:\n"); //allows user to select what to do with there messege
             printf("1 = Encrypt Message.\n");
             printf("2 = Decrypt Message.\n");
             scanf("%d", &input);
@@ -33,28 +32,27 @@ int main () {
     
         if(input == 1)  { //Encription
             for( n=0 ; n < strlen(s1) ; n++ )   {
+                f1[n] = s1[n];
                 if((s1[n] >= 65) && (s1[n] <= 90))  { //if a capital letter
-                    f1[n] = s1[n];
                     s1[n] = s1[n] - key;  //minuses the key given
                     if((s1[n]) < 65)    { 
                         s1[n] = s1[n] + 26; //allows for rollover if before A
                     }
                 }
                 if((s1[n] >= 97) && (s1[n] <= 122)) {
-                    f1[n] = s1[n];
                     s1[n] = s1[n] - key - 32;
                     if((s1[n]) < 65)    {
                         s1[n] = s1[n] + 26;
                     }
                 }
             }
-            printf("\nRecieved input is: %s\n", f1); //need to fix
-            printf("Your Encription is: %s\n", s1); //Prints Encription
+            printf("\nRecieved input is: %s\n", f1);
+            printf("\nYour Encription is: %s\n", s1); //Prints Encription
         }
         if(input == 2)  { //Decryption
             for( n=0 ; n < strlen(s1) ; n++ )   {
+                f1[n] = s1[n];
                 if((s1[n] >= 65) && (s1[n] <= 90))  { //If a capitial letter entered 
-                    f1[n] = s1[n];
                     s1[n] = s1[n] + key; // shifts number of spaces
                     if((s1[n]) > 90)    {
                         s1[n] = s1[n] - 26; //allows for rollover from Z back to A
@@ -69,26 +67,28 @@ int main () {
                 }
             }
             printf("\nRecieved input is: %s\n", f1);
-            printf("Your Decryption is: %s\n", s1); //prints the Decryption string to user
+            printf("\nYour Decryption is: %s\n", s1); //prints the Decryption string to user
         }
     }
     if(RS == 2) {
-        printf("Enter your message to encrypt or decrypt:\n");
+        printf("\nEnter your message to encrypt or decrypt:\n");
         scanf(" %[^\n]", s1 ); //user input
         printf("Enter Ciper code");
         scanf(" %[^\n]", s2 );
         do  {
-            printf("Select from options below:\n"); //allows user to select what to do with there messege
+            printf("\nSelect from options below:\n"); //allows user to select what to do with there messege
             printf("1 = Encrypt Message.\n");
             printf("2 = Decrypt Message.\n");
             scanf("%d", &input);
-        }   while(input < 1 || input > 2);
-        
-        if(input == 1) {
+        }   while(input < 1 || input >2);
+    if(input == 1) {
         
         
     }
     }
 }
 
+
+
+    
 
